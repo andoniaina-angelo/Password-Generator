00f07f9ec05ef80d694d1f0b7692b00ef2bc9885
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2025-11-19 11:15:59 +0100

    ajout utf-8 et sha256

--- password4.py
+++ password4.py
@@ -1,10 +1,13 @@
 def mdp():
+    import hashlib
     password=input("entrez votre mot de passe: ")
     if set(password) & set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")  and set(password) & set("abcefghijklmnopqrstuvwxyz") and set(password) & set("!@#$%^&*") and set(password) & set("0123456789") and len(password)>=8:
         print("mot de passe valide")
+        password= hashlib.sha256(password.encode("utf-8")).hexdigest()
+
     else:
         print("erreur: mot de passe insuffisant")
         mdp()
-mdp()
+    print(password) 
 
-         
+mdp()
