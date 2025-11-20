58e7696668183bcfaf8798caf15c282957547a04
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2025-11-20 09:51:30 +0100

    ajout mdp sans ecrasement

--- password4.py
+++ password4.py
@@ -1,22 +1,26 @@
 
 import hashlib
 import json
+import os
 def mdp():
-    
     password=input("entrez votre mot de passe: ")
-    
     if set(password) & set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")  and set(password) & set("abcefghijklmnopqrstuvwxyz") and set(password) & set("!@#$%^&*") and set(password) & set("0123456789") and len(password)>=8:
         print("mot de passe valide")
         password= [hashlib.sha256(password.encode("utf-8")).hexdigest()]
         
-        
-        
-        with open('password.json','w', encoding='utf-8') as fichier:
-                  json.dump(password,fichier,separators=(",", ":"))
+        if os.path.exists("datapassword.json") and os.path.getsize("datapassword.json") > 0:
+           with open("datapassword.json",'r') as f:
+               datapassword=json.load(f)
+        else:
+            datapassword
+        nouveau_mdp=password    
+        if nouveau_mdp:
+            datapassword.append(nouveau_mdp)
+        with open("datapassword.json",'w') as f:
+            json.dump(datapassword,f,ensure_ascii=False)
+            
     else:
         print("erreur: mot de passe insuffisant")
         mdp()
     print(password)
-     
-
 mdp()
